735dca6b41fba23b83cb5c6e8a1707e041c57b00
Sir Rolin <nicolaieno@msn.com>
2024-09-09 12:56:59 +0200

    gitignore setup

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    private GameObject currentPlayer;
+    public GameObject getCurrentPlayer()
+    {
+        return currentPlayer;
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        currentPlayer = Instantiate(playerPrefab, new Vector3(0,1,0), Quaternion.identity);
+        currentPlayer.GetComponent<HealthScript>().onDeath += OnDeath;
+    }
+
+    private void OnDeath(){
+        Destroy(currentPlayer);
+        SpawnPlayer();
+    }
+}
